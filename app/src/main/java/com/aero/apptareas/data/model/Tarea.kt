96e6fb2d69e13a0991b2af4b05cc5062b4ef2d27
JamesCordova <jcordovas@gmail.com>
2026-04-25 18:31:01 -0500

    Add Room entity annotation to Tarea data class and set id as primary key

--- app/src/main/java/com/aero/apptareas/data/model/Tarea.kt
+++ app/src/main/java/com/aero/apptareas/data/model/Tarea.kt
@@ -1,7 +1,12 @@
 package com.aero.apptareas.data.model
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "tareas")
 data class Tarea(
-    val id: Int,
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
     val titulo: String,
     val completada: Boolean = false
 )
