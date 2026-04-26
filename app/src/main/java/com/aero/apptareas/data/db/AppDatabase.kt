1f3bc6220a1e4b0f1cd7c90226c7e92a721a418c
JamesCordova <jcordovas@gmail.com>
2026-04-26 13:34:57 -0500

    Add AppDatabase class for Room database setup and Tarea entity management, boilterplate code

---
+++ app/src/main/java/com/aero/apptareas/data/db/AppDatabase.kt
@@ -0,0 +1,35 @@
+package com.aero.apptareas.data.db
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.aero.apptareas.data.model.Tarea
+import kotlinx.coroutines.InternalCoroutinesApi
+import kotlinx.coroutines.internal.synchronized
+
+@Database(
+    entities = [Tarea::class],
+    version = 1,
+    exportSchema = false
+)
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun tareaDao(): TareaDao
+
+    companion object {
+        @Volatile
+        private var instance: AppDatabase? = null
+
+        @OptIn(InternalCoroutinesApi::class)
+        fun getInstance(context: Context): AppDatabase {
+            return instance ?: synchronized(this) {
+                Room.databaseBuilder(
+                    context.applicationContext,
+                    AppDatabase::class.java,
+                    "app_tareas.db"
+                ).build().also { instance = it }
+            }
+        }
+    }
+}
+
