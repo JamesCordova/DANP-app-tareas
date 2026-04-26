3302011faa185916f6fa3d9044b83e2d4955f82b
JamesCordova <jcordovas@gmail.com>
2026-04-26 15:59:20 -0500

    Add material-icons-extended dependency to support icon usage in Compose

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,6 +55,7 @@ dependencies {
     implementation(libs.lifecycle.viewmodel.compose)
     implementation(libs.androidx.room.runtime)
     implementation(libs.androidx.room.ktx)
+    implementation(libs.androidx.compose.material.icons.extended)
     ksp(libs.androidx.room.compiler)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
