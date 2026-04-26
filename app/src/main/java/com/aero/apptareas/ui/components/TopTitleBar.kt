17cf2605825312c9dd854e49b3f5f09d34d90520
JamesCordova <jcordovas@gmail.com>
2026-04-26 16:20:37 -0500

    Add TopTitleBar component for displaying app title and theme toggle button

---
+++ app/src/main/java/com/aero/apptareas/ui/components/TopTitleBar.kt
@@ -0,0 +1,26 @@
+package com.aero.apptareas.ui.components
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import com.aero.apptareas.ui.theme.ThemeViewModel
+
+@Composable
+fun TopTitleBar(themeViewModel: ThemeViewModel) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        TituloApp()
+        BotonToggleTema(
+            isDarkMode = themeViewModel.isDarkMode,
+            onToggle = themeViewModel::toggleTheme
+        )
+    }
+
+}
